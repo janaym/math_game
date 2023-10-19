9c47c5dc5a05a5197d80bb5d8db4e236edff27c7
Janay Ma <janay.ma7@gmail.com>
2023-10-18 19:39:59 -0700

    create game class

---
+++ game.rb
@@ -0,0 +1,16 @@
+class Game 
+
+  attr_reader :answer
+  
+  def initialize
+    a = rand(1...20)
+    b = rand(1...20)
+    @question = "What is #{a} plus #{b} equal?"
+    @answer = a + b
+  end
+
+  def ask_question
+    puts @question
+  end
+
+end
